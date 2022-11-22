f2dab126f697589074acd41670040fdfc33c229f
lonkaars <loek@pipeframe.xyz>
2022-11-22 21:40:22 +0100

    fix compatibility

--- shared.py
+++ shared.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 def get_public_key():
   data = urllib.request.urlopen("https://www.ns.nl/reisplanner").read()
   soup = BeautifulSoup(data, features="lxml")
-  data = "\n".join(map(lambda x: x.text, soup.select('script:-soup-contains("mlabProductKey")')))
+  data = "\n".join(str(x) for x in soup.select('script:-soup-contains("mlabProductKey")'))
   return re.search(r'mlabProductKey: "([0-9a-f]{32})"', data).group(1)
 
 def read_file(filename):
